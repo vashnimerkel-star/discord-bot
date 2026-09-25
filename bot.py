a9a9b905d5666cf139213da7d52cd407832d3af1
vashnimerkel-star <vashni.merkel@gmail.com>
2026-09-25 16:43:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import discord
 from discord.ext import commands, tasks
 from flask import Flask
 
-# Веб-сервер для работы на Render 24/7
+# Веб-сервер для поддержки активности Render 24/7
 app = Flask('')
 
 
@@ -23,11 +23,15 @@ def keep_alive():
     Thread(target=run_web).start()
 
 
-# НАСТРОЙКИ
-TOKEN = os.getenv('TOKEN')  # Токен добавим позже в настройках Render
-CHANNEL_ID = 1549615447794520086  # Вставьте ваш ID канала
-MENTION = '<@ne_lordylol_59426> <@nonker.>'  # Замените на ваши теги или @everyone
-ALARM_TIMES = ['15:00']  # Время оповещений (ЧЧ:ММ)
+# Указываем часовой пояс UTC+5
+TZ_UTC5 = datetime.timezone(datetime.timedelta(hours=5))
+
+# --- НАСТРОЙКИ ---
+TOKEN = os.getenv('TOKEN')
+CHANNEL_ID = 123456789012345678  # Укажите ваш ID канала
+MENTION = '<@ne_lordylol_59426> <@nonker.>'  # Ваши теги или @everyone
+ALARM_TIMES = ['15:00']  # Время срабатывания (по UTC+5)
+# -----------------
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -36,13 +40,15 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Бот {bot.user.name} готова сосать ваши большие члены, мальчики🥰')
+    print(f'Бот {bot.user.name} запущен.')
     alarm_loop.start()
 
 
 @tasks.loop(seconds=30)
 async def alarm_loop():
-    now = datetime.datetime.now().strftime('%H:%M')
+    # Берём время именно по поясу UTC+5
+    now = datetime.datetime.now(TZ_UTC5).strftime('%H:%M')
+
     if now in ALARM_TIMES:
         channel = bot.get_channel(CHANNEL_ID)
         if channel:
